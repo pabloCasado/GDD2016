cddd8aaa378d966c0303e93f67a0d8c530946142
Lautaro <garcia.a.lautaro@gmail.com>
2016-05-13 00:51:23 +0000

    Agrego la funcionalidad de la pantalla de login
    Usa el sp programado por los chicos
      OJO -> El sp funciona mal y siempre devuelve 1

--- tp/src/WindowsFormsApplication1/Form1.cs
+++ tp/src/WindowsFormsApplication1/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApplication1;
 
 namespace WindowsFormsApplication1
 {
@@ -15,6 +17,25 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connection = new DBConnection().openConnection();
+
+            var command = new SqlCommand("HARDCOR.loggin", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add(new SqlParameter("@username", this.textBox1.Text));
+            command.Parameters.Add(new SqlParameter("@password", this.textBox2.Text));
+            var returnParameter = command.Parameters.Add("@ReturnVal", SqlDbType.Int);
+            returnParameter.Direction = ParameterDirection.ReturnValue;
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            var result = returnParameter.Value;
+            Console.WriteLine(result);
         }
     }
 }
