1910809582780f5fee5e68f74959f79bcae21317
Lautaro <garcia.a.lautaro@gmail.com>
2016-05-14 18:21:56 +0000

    Agrego el handler del click
    Que me había olvidado de setear (no me acostumbro a dónde está cada
    archivo en C#)

--- tp/src/WindowsFormsApplication1/Form1.Designer.cs
+++ tp/src/WindowsFormsApplication1/Form1.Designer.cs
@@ -54,6 +54,7 @@
             this.button1.TabIndex = 16;
             this.button1.Text = "Acceder";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
